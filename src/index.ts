59f474d183c7daf52749277ab45f8bd184f0b27b
Radislav Erokho <radislav.erokho@artezio.com>
2022-06-18 19:56:07 +0300

    feat: build app in index.js

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import Server from "./components/Server";
+import UsersRouter from "./routers/UsersRouter";
+import { API_URL } from "./constants";
+
+import config from "./configuration/config";
+
+const server = Server.getServer();
+
+const userRouter = UsersRouter.getController();
+
+server.setBaseUrl(API_URL);
+
+userRouter.init(server);
+
+server.start(config.port);
